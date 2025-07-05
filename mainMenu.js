aa083f5c98a0058b345c0de3e5abfc72b6026280
PabloMyLove <94645033+PabloMyDarling@users.noreply.github.com>
2025-07-05 14:47:53 +0300

    Update mainMenu.js

--- mainMenu.js
+++ mainMenu.js
@@ -1,17 +1,18 @@
-function setModeAndStartGame(clicks) {
-  localStorage.setItem("increaseClicksBy", clicks);
+function setModeAndStartGame(clicks, time) {
+  sessionStorage.setItem("increaseClicksBy", clicks);
+  sessionStorage.setItem("increaseTimeBy", time);
   window.location.href = "./game.html";
 }
 
 document.getElementById("easymodeButton").addEventListener("click", () => {
-  setModeAndStartGame(45);
+  setModeAndStartGame(50, 15);
 });
 
 document.getElementById("normalmodeButton").addEventListener("click", () => {
-  setModeAndStartGame(70);
+  setModeAndStartGame(75, 10);
 });
 
 document.getElementById("hardmodeButton").addEventListener("click", () => {
-  setModeAndStartGame(95);
+  setModeAndStartGame(100, 5);
 });
 
